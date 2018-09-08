a2fe48fb02468c1343b1d21917462eb2527c2959
vibhu mahendru <vibhumahendru@gmail.com>
2018-09-08 20:58:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay (musicians, inst) {
   var array = []
   for (var i=0;i<musicians.length;i++){
-    var x = `${musicians[i]} plays ${inst[i]`
+    var x = `${musicians[i]} plays ${inst[i]}`
     array.push(x)
   }
   return x
